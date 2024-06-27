5db51847bf6ef8012919632715ca347070327af4
yara <uraeel@gmail.com>
2024-06-27 13:19:34 -0400

    test

--- app.js
+++ app.js
@@ -14,12 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "https://yasblog.netlify.app",
-      "https://master--yasblog.netlify.app",
-      "https://yasblog.netlify.app/login"
-    ],
+    origin: ["http://localhost:3000", "https://yasblog.netlify.app"],
   })
 );
 
