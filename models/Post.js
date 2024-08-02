ea218231c8553dfbd190f55037a201f157137201
yara <uraeel@gmail.com>
2024-08-02 14:14:01 -0400

    test date 4

--- models/Post.js
+++ models/Post.js
@@ -8,7 +8,7 @@ const postSchema = new mongoose.Schema({
   author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   createdAt: { type: Date, default: Date.now },
   tags: { type: [String], required: true },
-  date: { type: Date, required: false }
+  date: { type: Date, required: true }
 });
 
 const Post = mongoose.model("Post", postSchema);
