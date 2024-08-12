482fac5b57bea95aba543fc3c823495e1f6c08be
yara <uraeel@gmail.com>
2024-08-12 14:29:58 -0400

    origin updated

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://choubeh.netlify.app"],
+    origin: ["http://localhost:3000", "https://choubeh.netlify.app", "https://yasaman.net"],
   })
 );
 
